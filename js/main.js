18d720f3c85d4dd246e17faccf1f0d4ec3bd9ca8
KeemHyun-A <129471948+KeemHyun-A@users.noreply.github.com>
2023-05-15 12:47:31 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,65 @@
+// $(document).read(function () {
+//   $("article").hover(function () {
+//     $(".tv1").mouseover(function () {
+//       $(this).css("background-color", "black");
+//     });
+//     $(".tv2").mouseover(function () {
+//       $(this).css("background-color", "black");
+//     });
+//     $(".tv3").mouseover(function () {
+//       $(this).css("background-color", "black");
+//     });
+//     $(".tv4").mouseover(function () {
+//       $(this).css("background-color", "black");
+//     });
+//   });
+// });
+
+$(document).ready(function () {
+  var current = 0;
+  var setIntervalId;
+  $(this).eq(current).addClass("on");
+
+  $(".btns li").on({
+    click: function () {
+      var tg = $(this);
+      var i = tg.index();
+
+      movie(i);
+
+      $(".btns li").removeClass("on");
+      $(this).addClass("on");
+    },
+  });
+  $("#main").on({
+    mouseover: function () {
+      clearInterval(setIntervalId);
+    },
+    mouseout: function () {
+      timer();
+    },
+  });
+
+  timer();
+  function timer() {
+    setIntervalId = setInterval(function () {
+      var n = current + 1;
+      if (n == 3) {
+        n = 0;
+      }
+      movie(n);
+      $(".btns li").removeClass("on");
+      $(".btns li").eq(n).addClass("on");
+    }, 3000);
+  }
+  function movie(i) {
+    if (current == i) return;
+    var currentEl = $("#main > ul > li").eq(current);
+    var nextEl = $("#main > ul > li").eq(i);
+
+    currentEl.css({ opacity: 1 }).stop().animate({ opacity: 0 });
+    nextEl.css({ opacity: 0 }).stop().animate({ opacity: 1 });
+
+    current = i;
+  }
+});
